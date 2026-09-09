cea1bad45648251c429dad96b33516bf99960377
Danush-006 <danushsuresh2005@gmail.com>
2026-09-09 14:49:14 +0530

    commit

--- Server.js
+++ Server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose'); 
+const cors = require('cors');
 const app = express();
+app.use(cors());
 app.use(express.json());
 mongoose.connect("mongodb+srv://danushsuresh2005_db_user:2BUkwkmL5vIdcfPq@cluster0.fcejvhd.mongodb.net/?appName=Cluster0")
 .then(()=>{
